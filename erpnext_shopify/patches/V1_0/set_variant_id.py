d6063120f93f65cf2bd84ee290dee888bb803492
Saurabh <saurabh6790@gmail.com>
2015-12-15 16:09:26 +0530

    [fixes] update patch to set variant id

--- erpnext_shopify/patches/V1_0/set_variant_id.py
+++ erpnext_shopify/patches/V1_0/set_variant_id.py
@@ -3,11 +3,16 @@
 
 from __future__ import unicode_literals
 import frappe
-from erpnext_shopify.utils import get_request
+from erpnext_shopify.utils import get_shopify_items
+from frappe.utils import cint
+from erpnext_shopify.exceptions import ShopifyError
+import requests.exceptions
 
 def execute():
 	shopify_settings = frappe.get_doc("Shopify Settings")
-	if shopify_settings.shopify_url:
+	shopify_items = get_item_list()
+	
+	if shopify_settings.shopify_url and shopify_items:
 		for item in frappe.db.sql("""select name, item_code, shopify_id, has_variants, variant_of from tabItem 
 			where sync_with_shopify=1 and shopify_id is not null""", as_dict=1):
 			
@@ -16,10 +21,16 @@ def execute():
 					where name = %s """, item.get("name"))
 				
 			elif not item.get("has_variants"):
-				try:
-					product = get_request('/admin/products/{}.json'.format(item.get("shopify_id")))['product']
-			
+				product = filter(lambda shopify_item: shopify_item['id'] == cint(item.get("shopify_id")), shopify_items)
+				
+				if product:
 					frappe.db.sql(""" update tabItem set variant_id=%s 
-						where name = %s """, (product["variants"][0]["id"], item.get("name")))
-				except:
-					pass
+						where name = %s """, (product[0]["variants"][0]["id"], item.get("name")))
+
+def get_item_list():
+	try:
+		return get_shopify_items()
+	except (requests.exceptions.HTTPError, ShopifyError) as e:
+		frappe.throw(_("Somthing went wrong"), e)
+		
+	
