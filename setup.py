de30fcf05510fc07f15c7d057b7d28b9bdde76cd
Nabin Hait <nabinhait@gmail.com>
2016-02-04 14:01:33 +0600

    bumped to version 1.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "1.1.1"
+version = "1.1.2"
 
 requirements = parse_requirements("requirements.txt", session="")
 
