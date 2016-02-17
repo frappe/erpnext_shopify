bfb4cdba3912c0b5bbe21c47ac6a0da714be6f6e
Saurabh <saurabh6790@gmail.com>
2016-02-17 15:33:13 +0530

    [fixes] fix to avoid mandatory field exceptions like WarehouseNotSet

--- erpnext_shopify/api.py
+++ erpnext_shopify/api.py
@@ -11,7 +11,7 @@ from .sync_orders import sync_orders
 from .sync_customers import sync_customers
 from frappe.utils.user import get_system_managers
 from .sync_products import sync_products, update_item_stock_qty
-from .utils import create_log_entry, disable_shopify_sync_on_exception
+from .utils import disable_shopify_sync_on_exception
 
 @frappe.whitelist()
 def sync_shopify():
@@ -23,14 +23,19 @@ def sync_shopify():
 
 @frappe.whitelist()
 def sync_shopify_resources():
-	shopify_settings = frappe.get_doc("Shopify Settings", "Shopify Settings")
+	shopify_settings = frappe.get_doc("Shopify Settings")
+	
 	if shopify_settings.enable_shopify:
 		try :
+			validate_shopify_settings(shopify_settings)
+			
 			sync_products(shopify_settings.price_list, shopify_settings.warehouse)
 			sync_customers()
 			sync_orders()
 			update_item_stock_qty()
+			
 			frappe.db.set_value("Shopify Settings", None, "last_sync_datetime", frappe.utils.now())
+			
 		except ShopifyError:
 			disable_shopify_sync_on_exception()
 			
@@ -43,3 +48,13 @@ def sync_shopify_resources():
 					
 	elif frappe.local.form_dict.cmd == "erpnext_shopify.api.sync_shopify":
 		frappe.throw(_("""Shopify connector is not enabled. Click on 'Connect to Shopify' to connect ERPNext and your Shopify store."""))
+
+def validate_shopify_settings(shopify_settings):
+	"""
+		This will validate mandatory fields and access token or app credentials 
+		by calling validate() of shopify settings.
+	"""
+	try:
+		shopify_settings.save()
+	except ShopifyError:
+		disable_shopify_sync_on_exception()
