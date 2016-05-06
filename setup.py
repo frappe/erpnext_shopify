9340b49445c8a6ad83d58765542fb6d12e3cfcfa
Nabin Hait <nabinhait@gmail.com>
2016-05-06 16:44:29 +0600

    bumped to version 1.5.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = "1.5.3"
+version = "1.5.4"
 
 requirements = parse_requirements("requirements.txt", session="")
 
